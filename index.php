eaf1578bff3060f3b1ce0b9c259e479e5d7e86c6
Ulisses Bonfim <ulissesmb@gmail.com>
2022-09-24 15:40:20 -0300

    Mostrando como funciona o CRUD

--- index.php
+++ index.php
@@ -4,17 +4,21 @@
 /** 
 *  comentario de varias linhas
 */
-
 ?>
 
 NOS VAMOS CRIAR UM CRUD DO PHP.
 
 Whats CRUD?? "O que é CRUD"??
 
-C -> CREATE (CRIAR / ADICIONAR REGISTRO)
-R -> READ (LER / MOSTRAR UM REGISTRO | CARREGAR UMA LISTA DE REGISTRO)
-U -> UPDATE (ATUALIZAR REGISTRO | ATUALIZAR UMA LISTA)
-D -> DELETE (REMOVER O REGISTRO SELECIONADO)
+C -> CREATE (CRIAR / ADICIONAR REGISTRO) (POST)
+R -> READ (LER / MOSTRAR UM REGISTRO | CARREGAR UMA LISTA DE REGISTRO) (GET)
+U -> UPDATE (ATUALIZAR REGISTRO | ATUALIZAR UMA LISTA) (PUT)
+D -> DELETE (REMOVER O REGISTRO SELECIONADO) (GET | POST)
+
+Informações para RESTFULL
+
+https://restfulapi.net/http-status-codes/
+https://medium.com/@9cv9official/what-are-get-post-put-patch-delete-a-walkthrough-with-javascripts-fetch-api-17be31755d28
 
 
 Exemplo::
@@ -22,4 +26,52 @@ https://github.com/glaucia86/projeto.crud.php/blob/master/index.php
 https://github.com/chapagain/crud-php-simple
 https://github.com/mayrloncarlos/crud-php-mysql
 https://github.com/MarcosEllys/crud-php
+
+
+Ideia Rual
+
+##
+# Portal de material de estudo
+### 
+
+# funcionalidade 01
+Cadastrar materia de leitura
+tipo ??
+texto | descrição ??
+tem imagem??
+
+#########################
+# APLICAÇÃO OU MÃO NA MASSA
+#######################
+
+CODIGO_UNICO_MATERIAL
+TIPO: (PDF / DOCX / MAGAZINE / NEWS_PAPER)
+DESCRIÇÃO (...)
+FOTO_PRODUTO
+
+GERAR TABELA VERDADE / TESTE DE MESA
+ID   | TIPO     |  DESCRICAO        |FOTO
+01   |PDF       |GRAU 1º - MOSAICO  |F98SD7F9SD7F98SD.JPG
+02   |PDF       |GRAU 1º - TERRA PL |F98SD7F9SD7F98SD.JPG
+03   |PDF       |GRAU 1º - KABALA   |F98SD7F9SD7F98SD.JPG
+04   |PDF       |GRAU 1º - KKKKKKK  |F98SD7F9SD7F98SD.JPG
+05   |PDF       |GRAU 1º - LLLLLLL  |F98SD7F9SD7F98SD.JPG
+05   |PDF       |GRAU 1º - LLLLLLL  |F98SD7F9SD7F98SD.JPG
+...
+011   |PDF       |GRAU 1º - LUZ     |F98SD7F9SD7F98SD.JPG
+
+
+
+CREATE -> "C" DO CRUD
+
+ID          -> SDF0SD98FSD90F890 (regex -> 0-9 | a-z | A-Z | ~0-9)
+TIPO        ->
+DESCRICAO   ->
+FOTO        ->
+
+
+INTEIRO = ( 1, 2, 3)
+FLOAT / DLOUGLE / DECIMAL = 2.1 | 0.1 | 2.334
+STRING / CHARACTER = 'raul01-teste@gmail.com' | "raul-02020202@gmail.com"
+LOB = 
 
