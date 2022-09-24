93bc6ba5f0a4468169732a51d3b40a35940e8196
Ulisses Bonfim <ulissesmb@gmail.com>
2022-09-24 14:44:36 -0300

    incluindo texto sobre CRUD

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
+//comentarios de uma linha
 
-echo "Meu projeto2";
+/** 
+*  comentario de varias linhas
+*/
 
 ?>
+
+NOS VAMOS CRIAR UM CRUD DO PHP.
+
+Whats CRUD?? "O que é CRUD"??
+
+C -> CREATE (CRIAR / ADICIONAR REGISTRO)
+R -> READ (LER / MOSTRAR UM REGISTRO | CARREGAR UMA LISTA DE REGISTRO)
+U -> UPDATE (ATUALIZAR REGISTRO | ATUALIZAR UMA LISTA)
+D -> DELETE (REMOVER O REGISTRO SELECIONADO)
+
+
+Exemplo::
+https://github.com/glaucia86/projeto.crud.php/blob/master/index.php
+https://github.com/chapagain/crud-php-simple
+https://github.com/mayrloncarlos/crud-php-mysql
+https://github.com/MarcosEllys/crud-php
+
