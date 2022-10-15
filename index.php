6f472907dfaa79db514355f204373c875c733cf8
Raul Alves <raulandersom@gmail.com>
2022-10-15 10:57:06 -0300

    inclusão tangerina

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once("./config.php");
 
 $req = $_SERVER['REQUEST_METHOD'];
 if( $req == "GET"){ //POST == GET - FALSE
-    $frutasd[] = ["banana","maca","laranja"];
+    $frutasd[] = ["banana","maca","laranja","tangerina"];
     mostrar($frutasd);
 }
  
