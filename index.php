2cfff996cd14929b2f408549a363a85e2b7108dc
Ulisses Bonfim <ulissesmb@gmail.com>
2022-10-15 09:57:37 -0300

    Incluindo logica para crud

--- index.php
+++ index.php
@@ -1,77 +1,31 @@
 <?php
-//comentarios de uma linha
 
-/** 
-*  comentario de varias linhas
-*/
+/*
+ echo "<pre>";
+ print_r($_SERVER);
+ echo "</pre>";
+ */
+
+
+$req = $_SERVER['REQUEST_METHOD']; //POST
+
+if( $req == "GET"){ //POST == GET - FALSE
+    echo "oi ". $req;
+}
+
+if($req == "POST"){ //POST == POST -TRUE
+    echo "oi ". $req;
+}
+
+if($req == "DELETE"){ //POST == DELETE - FALSE
+    echo "oi ". $req;
+}
+
+if($req == "PATCH"){ //GET == PATCH - FALSE
+    echo "oi ".$req;
+}
+
+ 
 ?>
-
-NOS VAMOS CRIAR UM CRUD DO PHP.
-
-Whats CRUD?? "O que é CRUD"??
-
-C -> CREATE (CRIAR / ADICIONAR REGISTRO) (POST)
-R -> READ (LER / MOSTRAR UM REGISTRO | CARREGAR UMA LISTA DE REGISTRO) (GET)
-U -> UPDATE (ATUALIZAR REGISTRO | ATUALIZAR UMA LISTA) (PUT)
-D -> DELETE (REMOVER O REGISTRO SELECIONADO) (GET | POST)
-
-Informações para RESTFULL
-
-https://restfulapi.net/http-status-codes/
-https://medium.com/@9cv9official/what-are-get-post-put-patch-delete-a-walkthrough-with-javascripts-fetch-api-17be31755d28
-
-
-Exemplo::
-https://github.com/glaucia86/projeto.crud.php/blob/master/index.php
-https://github.com/chapagain/crud-php-simple
-https://github.com/mayrloncarlos/crud-php-mysql
-https://github.com/MarcosEllys/crud-php
-
-
-Ideia Rual
-
-##
-# Portal de material de estudo
-### 
-
-# funcionalidade 01
-Cadastrar materia de leitura
-tipo ??
-texto | descrição ??
-tem imagem??
-
-#########################
-# APLICAÇÃO OU MÃO NA MASSA
-#######################
-
-CODIGO_UNICO_MATERIAL
-TIPO: (PDF / DOCX / MAGAZINE / NEWS_PAPER)
-DESCRIÇÃO (...)
-FOTO_PRODUTO
-
-GERAR TABELA VERDADE / TESTE DE MESA
-ID   | TIPO     |  DESCRICAO        |FOTO
-01   |PDF       |GRAU 1º - MOSAICO  |F98SD7F9SD7F98SD.JPG
-02   |PDF       |GRAU 1º - TERRA PL |F98SD7F9SD7F98SD.JPG
-03   |PDF       |GRAU 1º - KABALA   |F98SD7F9SD7F98SD.JPG
-04   |PDF       |GRAU 1º - KKKKKKK  |F98SD7F9SD7F98SD.JPG
-05   |PDF       |GRAU 1º - LLLLLLL  |F98SD7F9SD7F98SD.JPG
-05   |PDF       |GRAU 1º - LLLLLLL  |F98SD7F9SD7F98SD.JPG
-...
-011   |PDF       |GRAU 1º - LUZ     |F98SD7F9SD7F98SD.JPG
-
-
-
-CREATE -> "C" DO CRUD
-
-ID          -> SDF0SD98FSD90F890 (regex -> 0-9 | a-z | A-Z | ~0-9)
-TIPO        ->
-DESCRICAO   ->
-FOTO        ->
-
-
-INTEIRO = ( 1, 2, 3)
-FLOAT / DLOUGLE / DECIMAL = 2.1 | 0.1 | 2.334
-STRING / CHARACTER = 'raul01-teste@gmail.com' | "raul-02020202@gmail.com"
-LOB = 
-
+<br />
+<h1>Sempre vai mostrar!!</h1>
