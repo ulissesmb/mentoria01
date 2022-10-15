e7831d4b15f58ff82fb0316d4dbd0fee59151210
Ulisses Bonfim <ulissesmb@gmail.com>
2022-10-15 10:47:07 -0300

    Criando include do config

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+//Meu primeiro metodo para mostrar os arrays das variaveis
+function mostrar($print){
+    echo "<pre>";
+    print_r($print);
+    echo "</pre>";   
+ }
+
+?>
--- index.php
+++ index.php
@@ -1,37 +1,15 @@
 <?php
-
-//Meu primeiro metodo para mostrar os arrays das variaveis
-function mostrar($print){
-    echo "<pre>";
-    print_r($print);
-    echo "</pre>";   
- }
+include_once("./config.php");
 
 
- //chamando o metodo criado anteriormente
- mostrar($_ENV);
-
-
-
-$req = $_SERVER['REQUEST_METHOD']; //POST
+$req = $_SERVER['REQUEST_METHOD'];
 
 if( $req == "GET"){ //POST == GET - FALSE
-    echo "oi ". $req;
+    //mostrar($req);
 }
 
-if($req == "POST"){ //POST == POST -TRUE
-    echo "oi ". $req;
-}
-
-if($req == "DELETE"){ //POST == DELETE - FALSE
-    echo "oi ". $req;
-}
-
-if($req == "PATCH"){ //GET == PATCH - FALSE
-    echo "oi ".$req;
-}
 
  
 ?>
 <br />
-<h1>Sempre vai mostrar!!</h1>
+<h1>Server...</h1>
