19830c287508e9b0fe5214e784652780364f8c16
ulisses.bonfim <ulissesmb@gmail.com>
2022-11-05 15:41:49 -0300

    regra

--- index.php
+++ index.php
@@ -3,20 +3,21 @@ include_once("./config.php");
 
 $request = $_SERVER['REQUEST_METHOD'];
 
+$help = "HTTP/1.1 ";
+$erro1 = "503 Service Unavailable";
+$erroFatal = "500 Internal Server Error";
+$create = "201 Created";
+$sucesso = "200 OK";
 
-if($request == "GET"){
-    //reponse 204
-    header("HTTP/1.1 204 No Content");
-    //echo "Registro cadastrado com sucesso";
+if($request == "GET"){  
+    header($help . $erro1);
+} elseif ($request == "POST" ||$request == "PUT") {
+    header($help . $create);
+} elseif ($request == "DELETE") {
+    header($help . $sucesso);
+} else {
+    header($help . $erroFatal);
 }
-
-if($request == "POST"){
-    //reponse 201
-    header("HTTP/1.1 404 Not Found");
-    //header("HTTP/1.1 200 No Content");
-    echo "registro nao encontrado!";
-}
-
 
 
 ?>
